01233eb4fac77f948a00d060e30848ac9462752c
Peter <merrygoat@users.noreply.github.com>
2018-10-30 17:23:01 +0000

    Generalised number of species

--- gel_setup.py
+++ gel_setup.py
@@ -14,9 +14,10 @@ box_volume = num_particles/number_density
 side_len = box_volume ** (1/3)
 
 # Set up the particle diameters
-Num_Types = 7
-bin_edges = np.arange(1 - (3 * pd), 1 + (3 * pd) + pd, pd) - pd / 2.
-bin_centers = (np.arange(-3, 4) * pd) + 1
+num_types = 7
+half_num_types = np.floor(num_types/2)
+bin_edges = np.arange(1 - (half_num_types * pd), 1 + (half_num_types * pd) + pd, pd) - pd / 2.
+bin_centers = (np.arange(-half_num_types, half_num_types + 1) * pd) + 1
 
 # Set up the Gaussian from which particle species are drawn
 cumulative = norm.cdf(bin_edges, 1, pd)
@@ -46,22 +47,22 @@ with open(filename, 'w') as ouptut_file:
     ouptut_file.write("{} atoms\n\n".format(num_particles))
 
     # Write the box size
-    ouptut_file.write("{} atom types\n".format(Num_Types))
+    ouptut_file.write("{} atom types\n".format(num_types))
     ouptut_file.write("0 {:.6f} xlo xhi\n"
                       "0 {:.6f} ylo yhi\n"
                       "0 {:.6f} zlo zhi\n\n".format(side_len, side_len, side_len))
 
     # Write the particle masses, the particle types all have mass density = 1
     ouptut_file.write("Masses\n\n")
-    for i in range(Num_Types):
+    for i in range(num_types):
         ouptut_file.write("%d %g\n" % (i + 1, np.pi / 6 * bin_centers[i] ** 3))
     ouptut_file.write("\n")
 
     # Specify the interaction coefficient for the Morse potential:
     # in lammps's documentation they are: d0 alpha r0 cutoff
     ouptut_file.write("PairIJ Coeffs\n\n")
-    for type_1 in range(Num_Types):
-        for type_2 in range(type_1, Num_Types):
+    for type_1 in range(num_types):
+        for type_2 in range(type_1, num_types):
             diam_i = bin_centers[type_1]
             diam_j = bin_centers[type_2]
             mixed_diam = 0.5 * (diam_i + diam_j)
